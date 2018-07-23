4bbe06b223539e250ebd096992c6376fe45efb73
Oskar <oskar.sadlowski@gmail.com>
2018-07-23 20:03:38 +0200

    init commit

---
+++ src/main/java/com/ehr/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.ehr.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "CarStorage is working! Keep your belts tight!";
+    }
+}
